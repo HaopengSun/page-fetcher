1b4a995c628c8e4d7f0c09e9463b2578abfd88db
HaopengSun <sunhaopeng@gmail.com>
2021-01-13 13:37:30 +0000

    add edge case

--- fetcher.js
+++ fetcher.js
@@ -1,16 +1,37 @@
 const web = process.argv.slice(2);
 const request = require('request');
-var fs = require('fs');
+const fs = require('fs');
+const readline = require('readline');
+const website = web[0];
+const path = web[1]
 
-request(web[0], (error, response, body) => {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+request(website, (error, response, body) => {
   // Print the error if one occurred
   console.log('error:', error);
   // Print the response status code if a response was received
   console.log('statusCode:', response && response.statusCode);
-  // Print the HTML for the Google homepage.
-  // console.log('body:', body);
-  fs.appendFile(web[1], body, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+ 
+  // check if file is existing or not
+  if (response.statusCode === 200 && fs.existsSync(path)) {
+    rl.question('File existed, do you want to overwrite it? (y/n)', (answer) => {
+      if (answer === 'y'){
+        // save downloaded file
+        fs.appendFile(path, body, function (err) {
+          if (err) throw err;
+          console.log('Saved!');
+        });
+      }
+      rl.close();
+    });
+  }
+
+  if (response.statusCode !== 200){
+    console.log('visit restricted!')
+    rl.close();
+  }
 });
